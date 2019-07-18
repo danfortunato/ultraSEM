fbb6503091bad893c86aedf6f478507510b74649
Nick Hale <nickphale@gmail.com>
2019-07-18 14:53:42 +0200

    Store conversion matrices persistently for speed.

--- +util/convertmat.m
+++ +util/convertmat.m
@@ -6,10 +6,23 @@ function S = convertmat(n, k1, k2)
 %   C^{(K2 + 1)} basis, where C^{(K)} denotes ultraspherical polynomial
 %   basis with parameter K. If K2 < K1, S is the N-by-N identity matrix.
 
+persistent storeS
+
+if ( isempty(storeS) )
+    storeS = cell(2,2);
+else
+    if ( length(storeS{k1+1,k2+1}) == n )
+        S = storeS{k1+1, k2+1};
+        return
+    end
+end
+
 S = speye(n);
 for s = k1:k2
     S = spconvert(n, s) * S;
 end
+
+storeS{k1+1, k2+1} = S;
 
 end
 
@@ -20,6 +33,7 @@ function T = spconvert(n, lam)
 %   truncation gives back a matrix of size N x N.
 
 % Relation is: C_n^(lam) = (lam/(n+lam))(C_n^(lam+1) - C_{n-2}^(lam+1))
+
 if ( lam == 0 )
     dg = .5*ones(n-2,1);
     T = spdiags([1 0 ; .5 0 ; dg -dg], [0 2], n, n);
