59d5a271bbcf2b30c033a910f31f6cd5eca3936a
danfortunato <dan.fortunato@gmail.com>
2020-04-23 00:30:34 -0400

    Plot domains as wireframes with MESH()

---
+++ +ultraSEM/@Domain/mesh.m
@@ -0,0 +1,17 @@
+function varargout = mesh(T, varargin)
+%MESH   Plot an ULTRASEM.DOMAIN as a mesh.
+%   MESH(T) plots the ULTRASEM.DOMAIN T.
+
+if ( numel(T) > 1 ) 
+    holdState = ishold();
+    for k = 1:numel(T)
+        mesh(T(k), varargin{:}); hold on
+    end
+    if ( ~holdState )
+        hold off
+    end
+else
+    [varargout{1:nargout}] = mesh(T.domain, varargin{:});
+end
+
+end
---
+++ +ultraSEM/@Mapping/mesh.m
@@ -0,0 +1,23 @@
+function varargout = mesh(T, varargin)
+%MESH   Plot an ULTRASEM.MAPPING as a mesh.
+%   MESH(T) plots the ULTRASEM.MAPPING T as a mesh.
+
+holdState = ishold();
+
+hold on
+for k = 1:numel(T)
+    % Plot domain:
+    vertices = T(k).v;
+    h(k,1) = fill(vertices([1:end, 1],1), vertices([1:end, 1],2), ...
+        0*vertices([1:end, 1],1), 'FaceAlpha', 0, varargin{:});
+end
+
+if ( ~holdState )
+    hold off
+end
+
+if ( nargout > 0 )
+    varargout = {h};
+end
+
+end
