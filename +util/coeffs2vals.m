83d2e406c6ebe31a7f0928119b112d15b8cb22c9
danfortunato <dan.fortunato@gmail.com>
2020-05-21 12:28:15 -0400

    Do matrix multiplication for small transforms

--- +util/coeffs2vals.m
+++ +util/coeffs2vals.m
@@ -3,6 +3,29 @@ function values = coeffs2vals(coeffs)
 %
 % See also VALS2COEFFS.
 
-values = chebtech2.coeffs2vals(coeffs); % TODO: Remove chebfun
+% Store the coeffs2vals matrices for sizes < cutoff
+persistent F
+cutoff = 100;
+
+% Get the length of the input:
+n = size(coeffs, 1);
+
+if ( n <= 1 )
+    % Trivial case (constant):
+    values = coeffs;
+elseif ( n < cutoff )
+    % Use matrix multiplication for small problems
+    if ( isempty(F) )
+        F = cell(cutoff, 1);
+    end
+    if ( isempty(F{n}) )
+        x = util.chebpts(n);
+        F{n} = cos(acos(x) * (0:n-1));
+    end
+    values = F{n} * coeffs;
+else
+    % Use fast transform
+    values = chebtech2.coeffs2vals(coeffs); % TODO: Remove chebfun
+end
 
 end
--- +util/vals2coeffs.m
+++ +util/vals2coeffs.m
@@ -3,6 +3,29 @@ function coeffs = vals2coeffs(values)
 %
 % See also COEFFS2VALS.
 
-coeffs = chebtech2.vals2coeffs(values); % TODO: Remove chebfun
+% Store the vals2coeffs matrices for sizes < cutoff
+persistent F
+cutoff = 100;
+
+% Get the length of the input:
+n = size(values, 1);
+
+if ( n <= 1 )
+    % Trivial case (constant):
+    coeffs = values;
+elseif ( n < cutoff )
+    % Use matrix multiplication for small problems
+    if ( isempty(F) )
+        F = cell(cutoff, 1);
+    end
+    if ( isempty(F{n}) )
+        x = util.chebpts(n);
+        F{n} = inv( cos(acos(x) * (0:n-1)) ); % Is there a better way to do this than inv?
+    end
+    coeffs = F{n} * values;
+else
+    % Use fast transform
+    coeffs = chebtech2.vals2coeffs(values); % TODO: Remove chebfun
+end
 
 end
