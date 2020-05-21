5fe83ad7dd459f983ecdb6da6f312809ab0befb9
danfortunato <dan.fortunato@gmail.com>
2020-05-21 16:07:38 -0400

    Change syntax of compose() for multiple arguments

--- +ultraSEM/@Sol/compose.m
+++ +ultraSEM/@Sol/compose.m
@@ -1,11 +1,22 @@
-function sol = compose(sol, op)
+function f = compose(op, f, g)
 %COMPOSE   Compose a function handle with an ULTRASEM.SOL.
-%   COMPOSE(SOL, OP) returns an ULTRASEM.SOL representing OP(SOL), where
-%   SOL is also an ULTRASEM.SOL and OP is a function handle. The function
-%   handle OP is applied to SOL in value space.
+%   COMPOSE(OP, F) returns an ULTRASEM.SOL representing OP(F), where OP is
+%   a function handle and F is an ULTRASEM.SOL. The function handle OP is
+%   applied to F in value space.
+%
+%   COMPOSE(OP, F, G) returns an ULTRASEM.SOL representing OP(F, G), where
+%   OP is a function handle and F and G are ULTRASEM.SOLs. The function
+%   handle OP is applied to F and G in value space.
 
-vals = coeffs2vals(sol.u);
-vals = cellfun(op, vals, 'UniformOutput', false);
-sol.u = vals2coeffs(vals);
+ff = coeffs2vals(f.u);
+
+if ( nargin == 2 )
+    ff = cellfun(op, ff, 'UniformOutput', false);
+else
+    gg = coeffs2vals(g.u);
+    ff = cellfun(op, ff, gg, 'UniformOutput', false);
+end
+
+f.u = vals2coeffs(ff);
 
 end
